9bf02a4c3c037e1d6713f58039e93acd7f23301b
cma-desktop <yamanaka-team@hotmail.co.jp>
2020-08-01 00:23:07 +0900

    Setup repository

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Kosugi', sans-serif;
+        }
+    </style>
+    <title>OME Portal - @yield('title')</title>
+</head>
+<body>    
+    <div id="app" class="container">
+        @yield('content')
+    </div>
+
+    @yield('scripts')
+</body>
+</html>
