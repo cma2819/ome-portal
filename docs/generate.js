159020f61b128c91b0a2c90a7c6212a46e77ff69
cma-desktop <yamanaka-team@hotmail.co.jp>
2020-08-01 00:26:10 +0900

    Add generate swagger-codegen scripts

---
+++ docs/generate.js
@@ -0,0 +1,21 @@
+const { exec, execSync } = require('child_process')
+const { join } = require('path');
+const { mkdirSync, fstat, writeFileSync } = require('fs');
+
+module.exports.generate = async (input, lang) => {
+    if (!input || !lang) {
+        console.log('Bad arguments provided.');
+        return;
+    }
+
+    const jarPath = join(__dirname, 'swagger-codegen-cli.jar');
+    const outputDir = join(__dirname, 'output', lang);
+    mkdirSync(outputDir, {
+        recursive: true
+    });
+    writeFileSync(join(outputDir, '.swagger-codegen-ignore'));
+    console.log(`Code-generate to ${outputDir}`);
+    execSync(`java -jar ${jarPath} generate -i ${input} -l ${lang} -o ${outputDir}`);
+
+    return Promise.resolve(outputDir);
+}
