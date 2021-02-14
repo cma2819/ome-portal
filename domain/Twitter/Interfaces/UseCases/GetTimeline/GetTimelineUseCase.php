b03a899d1df159c7fab86432ce7a85c74c8cac5c
cma-desktop <yamanaka-team@hotmail.co.jp>
2021-02-14 15:36:00 +0900

    Commit change filename case

---
+++ domain/Twitter/Interfaces/UseCases/GetTimeline/GetTimelineUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ome\Twitter\Interfaces\UseCases\GetTimeline;
+
+interface GetTimelineUseCase
+{
+    /**
+     * Get user's timeline.
+     *
+     * @return GetTimelineResponse
+     */
+    public function interact(): GetTimelineResponse;
+}
